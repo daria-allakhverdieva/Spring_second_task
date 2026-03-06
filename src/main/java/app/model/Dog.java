8ce6bcc15dc13272f94c95886f2fa52ba1335072
Daria Allakhverdieva <daria@allakhverdieva.ru>
2026-03-06 15:20:40 +0300

    add Dog.java

---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,14 @@
+package app.model;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class Dog extends Animal {
+
+    @Override
+    public String toString() {
+        return "I'm a Dog";
+    }
+}
